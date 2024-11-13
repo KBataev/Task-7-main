135c3a6a37f1ee4d7b28326a2d4126d86c07e0bb
KBataev <b5968321b@mail.ru>
2024-11-14 01:47:36 +0300

    update username and password

--- Task-7-main/src/main/java/ru/itmentor/spring/boot_security/demo/controllers/HelloController.java
+++ Task-7-main/src/main/java/ru/itmentor/spring/boot_security/demo/controllers/HelloController.java
@@ -1,13 +1,24 @@
 package ru.itmentor.spring.boot_security.demo.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.itmentor.spring.boot_security.demo.models.Person;
+import ru.itmentor.spring.boot_security.demo.security.PersonDetails;
 
 @Controller
 public class HelloController {
     @GetMapping("/persons")
     public String allPersons() {
         return "persons";
+    }
+    @GetMapping("infoOneUser")
+    public String infoOneUser(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();//помещаем в контекст данного пользователя
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
+        return "index";
     }
 }
